2032fc7b499252e333f806a5741145cdd711d27e
Kincses-Tamas <kincses.tamas@students.jedlik.eu>
2022-10-14 11:34:36 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginAblakComponent } from './login-ablak/login-ablak.component';
+import { FejlecComponent } from './fejlec/fejlec.component';
+import { LablecComponent } from './lablec/lablec.component';
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginAblakComponent
+    LoginAblakComponent,
+    FejlecComponent,
+    LablecComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/login-ablak/login-ablak.component.ts
+++ src/app/login-ablak/login-ablak.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-ablak.component.css']
 })
 export class LoginAblakComponent implements OnInit {
+  
+  public labelUsername:string ="Felhasználónév:"
+  public labelPassword:string ="Jelszó:"
+  public labelSubmit:string ="Bejelentkezés"
+  public labelReset:string ="Adatok törlése"
+  public username:string = "Kovi Hari";
+  public password:string = "";
+  public error:string="";
 
-  constructor() { }
+  public loggedin:boolean=false;
 
+
+  constructor() { 
+    
+  }
+  
   ngOnInit(): void {
+    this.labelUsername="Username:"
+    this.labelPassword="Password:"
+    this.labelSubmit="Submit"
+    this.labelReset ="Reset"
+  }
+  logout(){
+    this.loggedin=false;
+    this.error="";
+    this.password="";
   }
 
+
+
+  kattint() {
+    this.error="";
+    this.loggedin=false;
+    if (this.username=="admin" && this.password=="123") {
+      this.loggedin=true;
+    }
+    else
+    this.error=("Hibás felhasználónév v jelszo");
+  }
 }
